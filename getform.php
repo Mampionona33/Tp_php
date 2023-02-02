3ab95f4cb0ff097388163c976a9be91d1e3032cd
Mampionona33 <ramamps33@gmail.com>
2023-02-02 21:34:01 +0300

    exercice 3

---
+++ getform.php
@@ -0,0 +1,10 @@
+<?php
+
+
+echo "Eto !!! <br>";
+
+// echo count($_GET);
+
+// print_r($_GET);
+
+echo  $_GET["email"];
--- page3.php
+++ page3.php
@@ -1,25 +1,16 @@
 <?php
 include("./krumo-0.4.4/class.krumo.php");
+
+extract($_GET);
+
+echo "Vous avez saisie: <hr/> email : $email; <br/> comment: $comment; <br/> list: $liste; <br/>";
+
 if (isset($_GET["checkbox"])) {
-    $checkbox = $_GET["checkbox"];
-}
-
-// if ($checkbox) {
-//     krumo($checkbox);
-// }
-
-echo "email : " . $_GET["email"] . "<br>";
-echo "Commentaire : " . $_GET["comment"] . "<br>";
-echo "Votre choix dans la liste : " . $_GET["liste"] . "<br>";
-
-if (isset($checkbox)) {
-    echo "Vos choix avec checkbox : ";
-    for ($i = 0; $i < count($checkbox); $i++) {
-        echo $checkbox[$i] . "; ";
+    echo "checkbox : ";
+    foreach ($_GET["checkbox"] as $val) {
+        echo "$val ;";
     }
-    echo "</br>";
-} else {
-    echo "Aucun valeur de checkbox n'est selectionner </br>";
+    echo "<br/>";
 }
-echo "Votre choix avec des boutons radio : " . $_GET["radioBtn"] . "<br>";
-echo "Le nom du fichier : " . $_GET["file"] . "<br>";
+
+echo "radioBtn : $radioBtn; <br/> file : $file; <hr/> ";

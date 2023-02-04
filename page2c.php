c4933c1c9c5ae28f871a45c8d4c3bb68bc48fa07
Mampionona33 <ramamps33@gmail.com>
2023-02-04 09:27:29 +0300

    remove all inused files

--- page2c.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Définition d'un tableau
-$tab = array("bonjour1", "bonjour2", "bonjour3", "bonjour4", "bonjour5", "bonjour6");
-echo "<pre>";
-print_r($tab);
-echo "</pre>";
--- page2d.php
+++
@@ -1,4 +0,0 @@
-<?php
-$tab = array("bonjour1", "bonjour2", "bonjour3", "bonjour4", "bonjour5", "bonjour6");
-include("./krumo-0.4.4/class.krumo.php");
-krumo($tab);

ff03b3570b34e3645edb7205d839aac0d30fff04
Mampionona33 <ramamps33@gmail.com>
2023-02-12 10:05:40 +0300

    finish exercice 1.7 exercice n°6 les formulaires

---
+++ page6.php
@@ -0,0 +1,24 @@
+<?php
+/*
+CREATE TABLE `exemple` (
+`description` varchar(150) NOT NULL default ''
+);
+*/
+// si formulaire envoyé
+if (isset($_POST['description'])) {
+    // connexion à la base de données
+    $bdd = mysqli_connect('localhost', 'root', '', 'test')
+        or die("Impossible de se connecter à la base de données");
+    mysqli_select_db($bdd, 'test')
+        or die("Impossible de sélectionner la base de données");
+    // protection des données
+    $description = mysqli_real_escape_string($bdd, $_POST['description']);
+    // insertion des données
+    $sql = "INSERT INTO `exemple` (`description`) VALUES ('" . $description . "')";
+    mysqli_query($bdd, $sql);
+    // fin
+    mysqli_close($bdd);
+}
+echo '<form method="POST" action="page6.html">
+<input type="submit" value="Recommencer" />
+</form>';

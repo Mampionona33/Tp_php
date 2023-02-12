6800fefcebc756b05350ae3f1609f1ee696e1d25
Mampionona33 <ramamps33@gmail.com>
2023-02-12 14:19:40 +0300

    finish exercice 2.3 Exercice n°3 boucle simple

--- page1.php
+++ page1.php
@@ -1,65 +1,34 @@
 <?php
+extract($_POST);
 
-// BOUCLE FOR INCREMENT
-echo 'Boucle "for" increment: <br/>';
-for ($a = 0; $a < 2; $a++) {
-    for ($i = 1; $i < 99; $i++) {
-        if ($i % 2) {
-            echo "$i - ";
+if ($pas > $debut && $pas > $fin) {
+    echo '
+    <p> Le pas doit être inferieur au debut et au fin </p> 
+    <form action="page1.html" method="post">
+        <input type="submit" value="Recommencer">
+    </form>
+        ';
+} else {
+    if ($debut == $fin) {
+        echo '
+        <p> le debut ne doit pas être égale à la fin !! </p> 
+        <form action="page1.html" method="post">
+            <input type="submit" value="Recommencer">
+        </form>
+            ';
+    } else if ($debut < $fin) {
+        echo '<form action="page1.html" method="post">
+            <input type="submit" value="Recommencer">
+            </form>';
+        for ($i = $debut; $i < $fin; $i += $pas) {
+            echo "$i -";
         }
-    }
-    echo "99";
-    echo "<hr/>";
-}
-echo "<hr/>";
-// -------------------------------------------
-
-// BOUCLE WHILE INCREMENT
-echo 'Boucle "while" increment: <br/>';
-$b = 0;
-while ($b < 2) {
-    $b++;
-    $c = 0;
-    while ($c < 98) {
-        $c++;
-        if ($c % 2) {
-            echo "$c - ";
-        }
-    };
-    echo "99";
-    echo "<hr/>";
-};
-echo "<hr/>";
-// -------------------------------------------
-
-// BOUCLE FOR DECREMENT
-echo 'Boucle "for" decrement: <br/>';
-for ($i = 0; $i < 2; $i++) {
-    for ($a = 100; $a > 2; $a--) {
-        if ($a % 3) {
-            echo "$a -";
-        }
-    }
-    echo "2";
-    echo "<hr/>";
-};
-echo "<hr/>";
-// -------------------------------------------
-
-// BOUCLE FOR DECREMENT
-echo 'Boucle "while" decrement: <br/>';
-$e = 0;
-while ($e < 2) {
-    $e++;
-    $f = 101;
-    while ($f > 3) {
-        $f--;
-        if ($f % 3) {
-            echo ("$f - ");
+    } else {
+        echo '<form action="page1.html" method="post">
+            <input type="submit" value="Recommencer">
+            </form>';
+        for ($i = $debut; $i > $fin; $i -= $pas) {
+            echo "$i -";
         }
     };
-    echo "2";
-    echo "<hr/>";
 };
-echo "<hr/>";
-// -------------------------------------------

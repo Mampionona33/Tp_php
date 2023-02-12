a81f5b49a51913e516287f878b27f6e6384704fa
Mampionona33 <ramamps33@gmail.com>
2023-02-12 16:48:40 +0300

    replace modulo by incremantation

--- page1.php
+++ page1.php
@@ -3,10 +3,8 @@
 // BOUCLE FOR INCREMENT
 echo 'Boucle "for" increment: <br/>';
 for ($a = 0; $a < 2; $a++) {
-    for ($i = 1; $i < 99; $i++) {
-        if ($i % 2) {
-            echo "$i - ";
-        }
+    for ($i = 1; $i < 99; $i += 2) {
+        echo "$i - ";
     }
     echo "99";
     echo "<hr/>";
@@ -19,12 +17,11 @@ echo 'Boucle "while" increment: <br/>';
 $b = 0;
 while ($b < 2) {
     $b++;
-    $c = 0;
+    $c = 1;
+    echo "$c - ";
     while ($c < 98) {
-        $c++;
-        if ($c % 2) {
-            echo "$c - ";
-        }
+        $c += 2;
+        echo "$c - ";
     };
     echo "99";
     echo "<hr/>";
@@ -35,10 +32,8 @@ echo "<hr/>";
 // BOUCLE FOR DECREMENT
 echo 'Boucle "for" decrement: <br/>';
 for ($i = 0; $i < 2; $i++) {
-    for ($a = 100; $a > 2; $a--) {
-        if ($a % 3) {
-            echo "$a -";
-        }
+    for ($a = 100; $a > 2; $a -= 2) {
+        echo "$a -";
     }
     echo "2";
     echo "<hr/>";
@@ -51,12 +46,11 @@ echo 'Boucle "while" decrement: <br/>';
 $e = 0;
 while ($e < 2) {
     $e++;
-    $f = 101;
-    while ($f > 3) {
-        $f--;
-        if ($f % 3) {
-            echo ("$f - ");
-        }
+    $f = 100;
+    echo "$f - ";
+    while ($f > 4) {
+        $f -= 2;
+        echo "$f - ";
     };
     echo "2";
     echo "<hr/>";

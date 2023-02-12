bc1d56c675b5e719b295d701844f12de60213786
Mampionona33 <ramamps33@gmail.com>
2023-02-12 20:04:35 +0300

    3_Exercice_n3_fonction_simple_avec_3_parametre_et_un_retour

---
+++ page1.php
@@ -0,0 +1,10 @@
+<?php
+
+function ajouteMAJ($ch1, $ch2, $ch3)
+{
+  return strtoupper($ch1) . " " . strtoupper($ch2) . " " . strtoupper($ch3);
+};
+
+$val = ajouteMAJ("test", "fdfsfs", "dfsfs");
+
+echo '$val vaut ' . $val;

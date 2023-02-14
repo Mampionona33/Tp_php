3ffa543e2afb7ca750fbcfad2e633ef8422de44f
Mampionona33 <ramamps33@gmail.com>
2023-02-14 22:50:34 +0300

    finish exo 4.2

--- page1.php
+++ page1.php
@@ -1,12 +1,19 @@
 <?php
 
 $text = file('text.txt', 0, null);
-$removedEmptyLine = [];
+$evenLine = [];
+$nombreLigne = 0;
 
 foreach ($text as $key => $value) {
-    if (strlen($value) > 2) {
-        array_push($removedEmptyLine, $value);
+    $nombreLigne++;
+    if (strlen($value)  % 2) {
+        array_push($evenLine, $value);
     }
 };
+echo '<h4> Le fichier original a : ' . $nombreLigne . ' lignes </h4><br/>';
+echo '<hr/>';
+echo '<h4> Le fichier de sortie a : ' . count($evenLine)  . ' lignes </h4><br/>';
 
-echo $removedEmptyLine[0];
+foreach ($evenLine as $key => $value) {
+    echo $evenLine[$key] . '<br/>';
+};

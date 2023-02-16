c9fcab9931c07742d74c5265e366a0267ae83cc7
Mampionona33 <ramamps33@gmail.com>
2023-02-16 20:47:42 +0300

    test

--- page1.php
+++ page1.php
@@ -1,3 +1,13 @@
 <?php
 
 $fileName = 'myCsv.csv';
+
+$list = array(
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+);
+
+if (!is_file("$fileName")) {
+    echo "file not exist";
+}

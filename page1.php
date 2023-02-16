5beea4584062417136a0b1060f377ec367fe7640
Mampionona33 <ramamps33@gmail.com>
2023-02-16 19:17:15 +0300

    finish exo 4.5

--- page1.php
+++ page1.php
@@ -1,10 +1,14 @@
 <?php
 
 $inputText = file_get_contents('input.txt', false, null, 0);
-
+$outputFile = "output.txt";
 
 if ($inputText) {
-    echo $inputText;
+    if (file_put_contents($outputFile, $inputText)) {
+        echo "Le fichier $outputFile a bien été créer !";
+    } else {
+        echo "erreur lors de la création du fichier $outputFile";
+    }
 } else {
     echo "the input file is empty";
 }

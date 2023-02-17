f70b7bc6323c2fe0c4d977b8e8614a80dd5247d0
mampionona33 <ramamps33@gmail.com>
2023-02-17 12:56:26 +0300

    test

--- page1.php
+++ page1.php
@@ -1,4 +1,8 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use Spatie\Async\Pool;
+
+$pool = Pool::create();
 
 $fileName = 'myCsv.csv';
 
@@ -10,4 +14,39 @@ $list = array(
 
 if (!is_file("$fileName")) {
     echo "file not exist";
+    if (file_put_contents($fileName, "")) {
+        $pool
+            ->add(function ($fileName, $list) {
+                echo "test";
+                $fp = fopen($fileName, 'w');
+                foreach ($list as $fields) {
+                    fputcsv($fp, $fields);
+                }
+                fclose($fp);
+            })
+            ->then(function () {
+                echo "file successfully created";
+            })
+            ->catch(function ($error) {
+                echo "$error";
+            });
+    }
 }
+
+
+// $pool = Pool::create();
+
+// foreach ($things as $thing) {
+//     $pool->add(function () use ($thing) {
+//         // Do a thing
+//         echo "test 1";
+//     })->then(function ($output) {
+//         // Handle success
+//         echo "test 2";
+//     })->catch(function (Throwable $exception) {
+//         // Handle exception
+//         echo "test 3";
+//     });
+// }
+
+// $pool->wait();

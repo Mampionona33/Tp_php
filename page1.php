3efff07179740dc54c7b0c19798d1e96d51b55e1
Mampionona33 <ramamps33@gmail.com>
2023-02-18 09:50:48 +0300

    correction

--- page1.php
+++ page1.php
@@ -9,4 +9,10 @@ foreach ($text as $key => $value) {
     }
 };
 
-echo $removedEmptyLine[0];
+echo '<p style="font-size: 1.5rem;">' . $removedEmptyLine[0] . '</p>';
+
+// Test avec file_get_content
+$text2 = file_get_contents('text.txt', false, null, 0);
+echo '<hr/>Test avec file_get_content text2 : <br/> <p style="font-size: 1.5rem;">' . $text2 . '</p>';
+var_dump($text2);
+// Test avec fopen

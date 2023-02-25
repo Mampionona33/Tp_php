d15aad85c167cba3d77cf2df82d4bd457dab4274
Mampionona33 <ramamps33@gmail.com>
2023-02-25 09:37:56 +0300

    correction

--- page1.php
+++ page1.php
@@ -2,17 +2,19 @@
 
 $text = file('text.txt', 0, null);
 $removedEmptyLine = [];
-
 foreach ($text as $key => $value) {
-    if (strlen($value) > 2) {
-        array_push($removedEmptyLine, $value);
-    }
+    array_push($removedEmptyLine, $value);
 };
+echo '<p style="font-size: 1.5rem;"> Test avec file :</br>' . $removedEmptyLine[0] . '</p>';
 
-echo '<p style="font-size: 1.5rem;">' . $removedEmptyLine[0] . '</p>';
+// Test avec fopen
+$text3 = fopen('text.txt', 'r');
+$contents = fgets($text3);
+echo '<hr/>Test avec fopen : <br/> <p style="font-size: 1.5rem;">' . $contents . '</p>';
+fclose($text3);
 
 // Test avec file_get_content
 $text2 = file_get_contents('text.txt', false, null, 0);
-echo '<hr/>Test avec file_get_content text2 : <br/> <p style="font-size: 1.5rem;">' . $text2 . '</p>';
-var_dump($text2);
-// Test avec fopen
+$first = explode(' ', $text2);
+var_dump($first);
+// echo '<hr/>Test avec file_get_content : <br/> <p style="font-size: 1.5rem;">' . $text2 . '</p>';

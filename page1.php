73e46f18eef2129ef088573b28470fcab18e277a
Mampionona33 <ramamps33@gmail.com>
2023-03-04 09:20:38 +0300

    finsih exercice remove first line

--- page1.php
+++ page1.php
@@ -5,9 +5,11 @@ $fileName = 'csv/monFichier.csv';
 $db = file($fileName, 0, null);
 
 foreach ($db as $key => $value) {
-  if (file_put_contents("csv/line_" . $key . ".txt", $value)) {
-    echo "line_" . $key . ".txt has been sucessully created <br/>";
-  } else {
-    echo "Error on creating" . $key . ".txt <br/>";
+  if ($key != 0) {
+    if (file_put_contents("csv/line_" . $key . ".txt", $value)) {
+      echo "line_" . $key . ".txt has been sucessully created <br/>";
+    } else {
+      echo "Error on creating" . $key . ".txt <br/>";
+    }
   }
 };

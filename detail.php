035af1d3e2b7898597fcb605664e7d5af90c0840
mampionona33 <ramamps33@gmail.com>
2023-03-20 11:37:58 +0300

    change button place

--- detail.php
+++ detail.php
@@ -29,12 +29,12 @@ if (isset($id)) {
 echo '
 <hr/>
 <div class="box">
-    <form action="page1.php" method="post" >
-        <input type="submit" value="Return" class="button success" >
-    </form>
     <form action="page1.php" method="POST" >
         <input type="hidden" name="delete_id" value="' . $id . '">
         <input type="submit" class="button" value="Delete" style="background-color: red; color: #fff; cursor:pointer">
+    </form>
+    <form action="page1.php" method="post" >
+        <input type="submit" value="Return" class="button success" >
     </form>
     <form action="" method="post" >
         <input class="button info" type="submit" value="PDF" >

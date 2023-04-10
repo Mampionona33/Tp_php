1f7809d5df56187e0ce74e7cd2398a28b686fc6d
Mampionona33 <ramamps33@gmail.com>
2023-04-10 16:13:26 +0300

    remove unused file

--- scripts/handleDeleteOne.js
+++
@@ -1,8 +0,0 @@
-function handleDeleteOne() {
-    
-}
-
-window.addEventListener("load", function () {
-  const delete_selected = document.getElementById("delete_selected");
-  delete_selected.addEventListener("click", handleSubmitDeleteSelect);
-});

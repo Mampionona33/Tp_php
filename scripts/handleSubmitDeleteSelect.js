45ad130c87f4d3d7de9febadc6f6965f35e746cf
Mampionona33 <ramamps33@gmail.com>
2023-04-10 17:39:11 +0300

    fix no selection, prompt confirmation for deletition

--- scripts/handleSubmitDeleteSelect.js
+++ scripts/handleSubmitDeleteSelect.js
@@ -1,16 +1,21 @@
 function handleSubmitDeleteSelect() {
-  if (
-    confirm("Êtes-vous sûr de vouloir supprimer les éléments sélectionnés ?")
-  ) {
-    const mainTableForm = document.getElementById("mainTableForm");
-    const delete_id = mainTableForm.querySelectorAll('input[name="delete_id"]');
-    for (let i = 0; i < delete_id.length; i++) {
-      const element = delete_id[i];
-      element.removeAttribute("name");
+  const mainTableForm = document.getElementById("mainTableForm");
+  const delete_id = mainTableForm.querySelectorAll('input[name="delete_id"]');
+  const delete_ids = mainTableForm.querySelectorAll(
+    'input[name="delete_ids[]"]:checked'
+  );
+  if (delete_ids.length > 0) {
+    if (
+      confirm("Êtes-vous sûr de vouloir supprimer les éléments sélectionnés ?")
+    ) {
+      for (let i = 0; i < delete_id.length; i++) {
+        const element = delete_id[i];
+        element.removeAttribute("name");
+      }
+      mainTableForm.setAttribute("method", "post");
+      mainTableForm.setAttribute("action", ".");
+      mainTableForm.submit();
     }
-    mainTableForm.setAttribute("method", "post");
-    mainTableForm.setAttribute("action", ".");
-    mainTableForm.submit();
   }
 }
 

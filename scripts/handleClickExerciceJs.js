f311b355eea3b6e77c70542192c316814175270a
mampionona33 <ramamps33@gmail.com>
2023-04-13 14:02:10 +0300

    finish handle click exercice button

--- scripts/handleClickExerciceJs.js
+++ scripts/handleClickExerciceJs.js
@@ -1,7 +1,9 @@
 const handleClickExerciceJs = () => {
   const navbarForm = document.getElementById("navbarForm");
   navbarForm.setAttribute("method", "post");
-  navbarForm.setAttribute("action", "pages/exercices_js.php");
+  const url = window.location;
+  const path = `${url.protocol}//${url.hostname}:3000/pages/exercices_js.php`;
+  navbarForm.setAttribute("action", path);
   console.log("exercice");
   navbarForm.submit();
 };

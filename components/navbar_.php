c8f7e4e3a7c12aa9ea72de965441bb3789051959
mampionona33 <ramamps33@gmail.com>
2023-04-13 12:04:04 +0300

    finish styling

--- components/navbar_.php
+++ components/navbar_.php
@@ -3,19 +3,27 @@ if (isset($_GET["id"])) {
     $id = $_GET["id"];
 } ?>
 
-<div class="sticky">
-    <form id="navbarForm" action="" method="">
+<div class="sticky ">
+    <form id="navbarForm" action="" method="" class="navbar">
         <?php if (isset($page) && preg_match("/index/i", $page)) { ?>
-            <input type="text" name="search" placeholder="Find" id="searchInput" class="input">
-            <input type="submit" value="search" id="submit_search" class="button primary">
-            <input type="button" value="Add new" id="add_new" class="button primary">
-            <input class="button secondary" id="download_pdf_list" type="button" value="Download PDF" name="download_pdf" onclick="this.form.action='pdf_list.php'">
-            <input class="button secondary" id="preview_pdf_list" type="button" value="Preview PDF" name="preview_pdf" onclick="this.form.action='./pages/pdf_list.php'">
+            <div class="right">
+                <input type="text" name="search" placeholder="Find" id="searchInput" class="input">
+                <input type="submit" value="search" id="submit_search" class="button primary">
+                <input type="button" value="Add new" id="add_new" class="button primary">
+            </div>
+            <div class="left">
+                <input class="button secondary" id="download_pdf_list" type="button" value="Download PDF" name="download_pdf" onclick="this.form.action='pdf_list.php'">
+                <input class="button secondary" id="preview_pdf_list" type="button" value="Preview PDF" name="preview_pdf" onclick="this.form.action='./pages/pdf_list.php'">
+            </div>
         <?php } else if (isset($page) && preg_match("/detail/i", $page)) { ?>
-            <input type="button" id="buttonList" value="List" class="button info">
-            <input class="button primary" id="editButton" type="button" data-id="<?php echo $id ?>" value="Edit" name="action">
-            <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Download PDF" id="download_pdf_detail" name="download_pdf">
-            <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Preview PDF" id="preview_pdf_detail" name="preview_pdf">
+            <div class="right">
+                <input type="button" id="buttonList" value="List" class="button info">
+                <input class="button primary" id="editButton" type="button" data-id="<?php echo $id ?>" value="Edit" name="action">
+            </div>
+            <div class="left">
+                <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Download PDF" id="download_pdf_detail" name="download_pdf">
+                <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Preview PDF" id="preview_pdf_detail" name="preview_pdf">
+            </div>
         <?php } ?>
     </form>
 </div>

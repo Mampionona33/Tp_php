946c1d59e91824eb8354c6723a4e3ac3917eadef
mampionona33 <ramamps33@gmail.com>
2023-04-13 13:21:06 +0300

    create exercice button

--- components/navbar_.php
+++ components/navbar_.php
@@ -14,16 +14,24 @@ if (isset($_GET["id"])) {
             <div class="left">
                 <input class="button secondary" id="download_pdf_list" type="button" value="Download PDF" name="download_pdf" onclick="this.form.action='pdf_list.php'">
                 <input class="button secondary" id="preview_pdf_list" type="button" value="Preview PDF" name="preview_pdf" onclick="this.form.action='./pages/pdf_list.php'">
-            </div>
-        <?php } else if (isset($page) && preg_match("/detail/i", $page)) { ?>
-            <div class="right">
-                <input type="button" id="buttonList" value="List" class="button info">
-                <input class="button primary" id="editButton" type="button" data-id="<?php echo $id ?>" value="Edit" name="action">
-            </div>
-            <div class="left">
-                <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Download PDF" id="download_pdf_detail" name="download_pdf">
-                <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Preview PDF" id="preview_pdf_detail" name="preview_pdf">
+                <?php if (isset($page) && $page != "exercice") { ?>
+                    <input type="button" value="Exercices" class="button primary">
             </div>
         <?php } ?>
+
+    <?php } else if (isset($page) && preg_match("/detail/i", $page)) { ?>
+        <div class="right">
+            <input type="button" id="buttonList" value="List" class="button info">
+            <input class="button primary" id="editButton" type="button" data-id="<?php echo $id ?>" value="Edit" name="action">
+        </div>
+        <div class="left">
+            <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Download PDF" id="download_pdf_detail" name="download_pdf">
+            <input class="button secondary" type="button" data-id="<?php echo $id ?>" value="Preview PDF" id="preview_pdf_detail" name="preview_pdf">
+            <?php if (isset($page) && $page != "exercice") { ?>
+                <input type="button" value="Exercices" class="button primary">
+        </div>
+    <?php } ?>
+
+<?php } ?>
     </form>
 </div>

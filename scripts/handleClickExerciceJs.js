1dacc09048792bdc4db2216bf94099fc18f7a96e
Mampionona33 <ramamps33@gmail.com>
2023-04-13 20:28:56 +0300

    finish handleClickExerciceJs

--- scripts/handleClickExerciceJs.js
+++ scripts/handleClickExerciceJs.js
@@ -2,9 +2,15 @@ const handleClickExerciceJs = () => {
   const navbarForm = document.getElementById("navbarForm");
   navbarForm.setAttribute("method", "post");
   const url = window.location;
-  const path = `${url.protocol}//${url.hostname}:3000/pages/exercices_js.php`;
+
+  let path;
+  if (url.pathname.includes("Tp_php")) {
+    path = `${url.protocol}//${url.hostname}/Tp_php/pages/exercices_js.php`;
+  } else {
+    path = `${url.protocol}//${url.hostname}:3000/pages/exercices_js.php`;
+  }
+
   navbarForm.setAttribute("action", path);
-  console.log("exercice");
   navbarForm.submit();
 };
 

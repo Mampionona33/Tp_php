44aeb6eb0800c96ade2841905a944ee2289bbfb2
Mampionona33 <ramamps33@gmail.com>
2023-04-22 09:29:25 +0300

    exercice 5.1 using pdo

--- index.php
+++ index.php
@@ -7,36 +7,49 @@ $password = "";
 $dbname = "test";
 $table = "exemple";
 
+$sql = "SELECT * FROM $table";
+
+try {
+    $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+    foreach ($dbh->query($sql) as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
 // Connexion à la base de données
-$mysqli = new mysqli($host, $username, $password, $dbname);
+// $mysqli = new mysqli($host, $username, $password, $dbname);
 
 // Vérification des erreurs de connexion
-if ($mysqli->connect_error) {
-    die('Erreur de connexion (' . $mysqli->connect_errno . ') '
-        . $mysqli->connect_error);
-}
-echo nl2br("Connect successfully to the database : $dbname <hr/> \n");
+// if ($mysqli->connect_error) {
+//     die('Erreur de connexion (' . $mysqli->connect_errno . ') '
+//         . $mysqli->connect_error);
+// }
+// echo nl2br("Connect successfully to the database : $dbname <hr/> \n");
 
 
 // Requête SQL
-$sql = "SELECT * FROM $table";
+// $sql = "SELECT * FROM $table";
 
 // Exécution de la requête
-if ($result = $mysqli->query($sql)) {
+// if ($result = $mysqli->query($sql)) {
     // Traitement des résultats
-    echo nl2br('Select table' . ' "' . $table . '"' . ": \n");
-    while ($row = $result->fetch_row()) {
-        echo nl2br("{$row[0]}\n");
-    }
+    // echo nl2br('Select table' . ' "' . $table . '"' . ": \n");
+    // while ($row = $result->fetch_row()) {
+    //     echo nl2br("{$row[0]}\n");
+    // }
     // Libération des résultats
-    $result->free();
-} else {
+    // $result->free();
+// } else {
     // Gestion des erreurs d'exécution de la requête
-    echo "Erreur: " . $mysqli->error;
-}
+    // echo "Erreur: " . $mysqli->error;
+// }
 
 // Fermeture de la connexion
-$mysqli->close();
+// $mysqli->close();
 
 // $link = mysqli_connect('localhost', 'root', '')
 // or die("Impossible de se connecter : " . mysqli_error($link));
